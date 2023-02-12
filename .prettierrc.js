5ec772df18cdfbd3b777889afca9517e5047e077
Annie Sun <anniesun@Annies-MacBook-Pro.local>
2023-02-12 18:19:54 -0500

    fix heroku

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,5 +3,5 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   printWidth: 80,
-  singleQuote: false,
+  singleQuote: true,
 };
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -6,47 +6,47 @@ const theme = createTheme({
       fontFamily: "Poppins",
       fontSize: 72,
       fontWeight: 700,
-      lineHeight: 1
+      lineHeight: 1,
     },
     header2: {
       fontFamily: "Poppins",
       fontSize: 64,
-      fontWeight: 700
+      fontWeight: 700,
     },
     header3: {
       fontFamily: "Poppins",
       fontSize: 48,
-      fontWeight: 700
+      fontWeight: 700,
     },
     header4: {
       fontFamily: "Poppins",
       fontSize: 32,
-      fontWeight: 700
+      fontWeight: 700,
     },
     header5: {
       fontFamily: "Poppins",
-      fontSize: 32
+      fontSize: 32,
     },
     body1: {
       fontFamily: "Nunito Sans",
       fontSize: 22,
-      fontWeight: 400
+      fontWeight: 400,
     },
     body2: {
       fontFamily: "Nunito Sans",
       fontSize: 16,
-      fontWeight: 400
+      fontWeight: 400,
     },
     button: {
       fontFamily: "Poppins",
       fontSize: 24,
-      textTransform: "normal"
-    }
+      textTransform: "normal",
+    },
   },
   shadows: [
     "none",
     "0px 5px 50px 5px rgba(0,0,0,0.04)",
-    "0px 5px 50px 5px rgba(0,0,0,0.1)"
+    "0px 5px 50px 5px rgba(0,0,0,0.1)",
   ],
   components: {
     MuiButton: {
@@ -55,7 +55,7 @@ const theme = createTheme({
           padding: "4px 24px",
           borderRadius: "10px",
           background:
-            "linear-gradient(150deg, rgba(88,165,255,1) 0%, rgba(96,228,221,1) 100%)"
+            "linear-gradient(150deg, rgba(88,165,255,1) 0%, rgba(96,228,221,1) 100%)",
         },
         outlined: {
           padding: "4px 24px",
@@ -64,30 +64,30 @@ const theme = createTheme({
           borderRadius: "10px",
           "&:hover": {
             borderColor: "#696969",
-            color: "#696969"
-          }
+            color: "#696969",
+          },
         },
         text: {
           padding: "4px 24px",
           background: "rgba(256,256,256,0.3)",
           color: "white",
-          borderRadius: "10px"
-        }
+          borderRadius: "10px",
+        },
       },
       defaultProps: {
         disableRipple: true,
         disableElevation: true,
-        variant: "contained"
-      }
+        variant: "contained",
+      },
     },
     MuiIconButton: {
       defaultProps: {
         disableRipple: true,
         disableElevation: true,
-        variant: "contained"
-      }
-    }
-  }
+        variant: "contained",
+      },
+    },
+  },
 });
 
 export default theme;
