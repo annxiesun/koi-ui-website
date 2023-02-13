35923dd06167d6468758928190ef475fea7c5a8f
Annie Sun <anniesun@Annies-MacBook-Pro.local>
2023-02-12 19:45:39 -0500

    add doc links

--- src/components/content/Header/index.js
+++ src/components/content/Header/index.js
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import { Grid, Typography, Button, IconButton } from "@mui/material";
-import { Github, Clipboard, Checkmark } from "../../widgets/Icons";
+import { Grid, Typography, Button, IconButton } from '@mui/material';
+import { Github, Clipboard, Checkmark } from '../../widgets/Icons';
 
 function Header() {
-  const npmCommand = "npm i @react-native-koi-ui";
+  const npmCommand = 'npm i @react-native-koi-ui';
   const [copied, setCopied] = useState(false);
 
   const copyNPM = () => {
@@ -18,7 +18,8 @@ function Header() {
       container
       alignContent="center"
       height="100vh"
-      sx={{ maxHeight: "900px" }}>
+      sx={{ maxHeight: '900px' }}
+    >
       <Grid marginBottom={10} xs={9}>
         <Typography variant="header1">{text[0]}</Typography>
         <Typography variant="body1">{text[1]}</Typography>
@@ -27,7 +28,8 @@ function Header() {
         container
         flexDirection="row"
         justifyContent="flex-start"
-        alignItems="center">
+        alignItems="center"
+      >
         <Grid
           onClick={copyNPM}
           container
@@ -36,22 +38,23 @@ function Header() {
           alignItems="center"
           justifyContent="space-between"
           sx={() => ({
-            borderRadius: "14px",
-            padding: "8px 16px",
-            backgroundColor: "#f2f2f2",
-            transition: "0.2s",
-            border: "#bababa 1px solid",
-            color: "#595959",
-            "&:hover": {
+            borderRadius: '14px',
+            padding: '8px 16px',
+            backgroundColor: '#f2f2f2',
+            transition: '0.2s',
+            border: '#bababa 1px solid',
+            color: '#595959',
+            '&:hover': {
               //boxShadow: theme.shadows[1],
-              transition: "0.2s",
-              backgroundColor: "#FFF",
-              border: "#58A5FF 1px solid",
-              color: "#58A5FF"
-            }
+              transition: '0.2s',
+              backgroundColor: '#FFF',
+              border: '#58A5FF 1px solid',
+              color: '#58A5FF',
+            },
           })}
-          variant="body1">
-          <Typography sx={{ fontFamily: "Poppins" }} variant="body2">
+          variant="body1"
+        >
+          <Typography sx={{ fontFamily: 'Poppins' }} variant="body2">
             {npmCommand}
           </Typography>
           <IconButton>
@@ -62,13 +65,20 @@ function Header() {
             )}
           </IconButton>
         </Grid>
-        <Button sx={{ margin: "0 16px" }}>{"Get Started"}</Button>
+        <Button
+          sx={{ margin: '0 16px' }}
+          href="https://defiant-airbus-410.notion.site/Docs-a3cdea7bfb6648d2b5838462d4fb8d82"
+          target="_blank"
+        >
+          {'Get Started'}
+        </Button>
         <Button
           endIcon={<Github fill="grey" height={25} width={25} />}
           variant="outlined"
           target="_blank"
-          href="https://github.com/annxiesun/KoiUI">
-          {"Github"}
+          href="https://github.com/annxiesun/KoiUI"
+        >
+          {'Github'}
         </Button>
       </Grid>
     </Grid>
@@ -78,8 +88,8 @@ function Header() {
 Header.propTypes = {};
 
 const text = [
-  "Create Seamless Mobile Experiences",
-  "Koi UI is the first fully customizable, mobile-first React Native Library"
+  'Create Seamless Mobile Experiences',
+  'Koi UI is the first fully customizable, mobile-first React Native Library',
 ];
 
 export default Header;
--- src/components/widgets/Navbar/index.js
+++ src/components/widgets/Navbar/index.js
@@ -1,48 +1,56 @@
-import React from "react";
+import React from 'react';
 
-import { Grid, IconButton, Button, CardMedia } from "@mui/material";
-import { Github, Discord } from "../Icons";
+import { Grid, IconButton, Button, CardMedia } from '@mui/material';
+import { Github, Discord } from '../Icons';
 
 function Navbar() {
   return (
     <Grid
       sx={() => ({
-        backgroundColor: "#FFF",
-        borderRadius: "24px",
-        position: "fixed",
-        top: "0px",
-        left: "50%",
-        width: "100%",
+        backgroundColor: '#FFF',
+        borderRadius: '24px',
+        position: 'fixed',
+        top: '0px',
+        left: '50%',
+        width: '100%',
         zIndex: 90,
-        boxSizing: "border-box",
-        transform: "translateX(-50%)"
+        boxSizing: 'border-box',
+        transform: 'translateX(-50%)',
         //boxShadow: theme.shadows[1]
       })}
       padding={2}
       container
       alignContent="center"
       alignItems="center"
-      justifyContent="space-between">
+      justifyContent="space-between"
+    >
       <CardMedia
         component="img"
-        sx={{ height: "50px", width: "auto" }}
-        src={"resources/images/logo.svg"}
+        sx={{ height: '50px', width: 'auto' }}
+        src={'resources/images/logo.svg'}
       />
       <Grid>
         <IconButton
-          sx={{ marginLeft: "8px" }}
+          sx={{ marginLeft: '8px' }}
           target="_blank"
-          href="https://github.com/annxiesun/KoiUI">
+          href="https://github.com/annxiesun/KoiUI"
+        >
           <Github fill="grey" height="30px" width="30px" />
         </IconButton>
         <IconButton
-          sx={{ marginLeft: "8px" }}
+          sx={{ marginLeft: '8px' }}
           target="_blank"
-          href="https://discord.gg/EGqK4BfB">
+          href="https://discord.gg/EGqK4BfB"
+        >
           <Discord fill="grey" height="30px" width="30px" />
         </IconButton>
-        <Button sx={{ fontSize: "16px", marginLeft: "8px" }} variant="outlined">
-          {"Docs"}
+        <Button
+          sx={{ fontSize: '16px', marginLeft: '8px' }}
+          variant="outlined"
+          href="https://defiant-airbus-410.notion.site/Docs-a3cdea7bfb6648d2b5838462d4fb8d82"
+          target="_blank"
+        >
+          {'Docs'}
         </Button>
       </Grid>
     </Grid>
